1249d84de4d5a0582223acf5368ec35c47d43f4f
liRenhao <renhao.li@bjyada.com>
2016-12-07 16:50:17 +0800

    修改Tabs

--- src/containers/Tabs.js
+++ src/containers/Tabs.js
@@ -6,30 +6,26 @@ import {
     Toolbar
 } from 'react-onsenui'
 
+const renderToolbar = (title) => {
+    return () => (
+        <Toolbar>
+            <div className='center'>{title}</div>
+        </Toolbar>
+    )
+}
+
 const renderTabs = () => {
     return [
         {
-            content: <Page key="home" renderToolbar={() => (
-                <Toolbar>
-                    <div className='center'>首页</div>
-                </Toolbar>)
-            }>首页</Page>,
+            content: <Page key="home" renderToolbar={renderToolbar("首页")}>首页</Page>,
             tab: <Tab key="home" label="首页" icon="ion-home"/>
         },
         {
-            content: <Page key="post" renderToolbar={() => (
-                <Toolbar>
-                    <div className='center'>发布</div>
-                </Toolbar>)
-            }>发布</Page>,
+            content: <Page key="post" renderToolbar={renderToolbar("发布")}>发布</Page>,
             tab: <Tab key="post" label="发布" icon="ion-plus"/>
         },
         {
-            content: <Page key="mine" renderToolbar={() => (
-                <Toolbar>
-                    <div className='center'>我的</div>
-                </Toolbar>)
-            }>我的</Page>,
+            content: <Page key="mine" renderToolbar={renderToolbar("我的")}>我的</Page>,
             tab: <Tab key="mine" label="我的" icon="ion-person"/>
         }
     ]
