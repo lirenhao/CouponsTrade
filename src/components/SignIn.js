e240f4cd8d7603fb95c75975f634148842a1c5b8
liRenhao <renhao.li@bjyada.com>
2016-12-08 09:58:40 +0800

    用户登录组件

---
+++ src/components/SignIn.js
@@ -0,0 +1,64 @@
+/**
+ * Author：liRenhao
+ * Create Date：2016/12/8
+ * Modified By：liRenhao
+ * Why & What is modified  <修改原因描述>
+ * 登录的组件
+ */
+import React from 'react'
+import {Field, reduxForm} from 'redux-form'
+import {Input, Button} from 'react-onsenui'
+
+const SignInField = ({input, type, placeholder}) => {
+    return (
+        <Input {...input}
+               type={type}
+               placeholder={placeholder}
+               modifier='underbar'
+               float/>
+    )
+}
+
+const SignIn = (props) => {
+    const {handleSubmit, onSubmit, invalid, submitting} = props
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <section style={{textAlign: 'center'}}>
+                <p>
+                    <Field type="number"
+                           name="username"
+                           component={SignInField}
+                           placeholder="Username"/>
+                </p>
+                <p>
+                    <Field type="password"
+                           name="password"
+                           component={SignInField}
+                           placeholder="Password"/>
+                </p>
+                <p>
+                    <button className="button" type="submit" disabled={invalid || submitting}>Sign in</button>
+                </p>
+            </section>
+        </form>
+    )
+}
+
+SignIn.propTypes = {
+    onSubmit: React.PropTypes.func.isRequired
+}
+
+const validate = (value) => {
+    const errors = {}
+    if (!value.username) {
+        errors.username = 'Required'
+    }
+    if (!value.password) {
+        errors.password = 'Required'
+    }
+    return errors
+}
+
+export default reduxForm({
+    form: "signIn", validate
+})(SignIn)
