721c920c7ba02689319cc4e150a2845cf6f4c387
kongli860329@163.com <kongli860329@163.com>
2016-12-14 14:53:24 +0800

    添加查看“發佈優惠券詳情”頁面

---
+++ src/containers/PublishComponDetail.js
@@ -0,0 +1,47 @@
+/**
+ * Author：likon
+ * Create Date：2016/12/14
+ * Modified By：likon
+ * Why & What is modified  <修改原因描述>
+ * 发布优惠券列表中查看优惠券详情容器
+ */
+import React from 'react'
+import {Page, Toolbar, BackButton} from 'react-onsenui'
+import CouponDetail from '../components/CouponDetail'
+import EditButton from '../components/Button'
+import SoldoutButton from '../components/Button'
+
+class ViewCouponDetail extends React.Component {
+    render() {
+        return (
+            <Page renderToolbar={() => (
+                <Toolbar>
+                    <div className='left'>
+                        <BackButton/>
+                    </div>
+                    <div className='center'>优惠券详细信息</div>
+                </Toolbar>
+            )}>
+                    <CouponDetail>
+                        DetailInformation={{
+                        applyCity: "该券仅适用北京",
+                        nickname: "small_cat",
+                        originalPrice: "￥50",
+                        sellingPrice: "￥70",
+                        couponName: "西提厚牛排优惠券",
+                        isAutomaticRefund: "是",
+                        couponType: "西餐",
+                        ticketPrice: "￥100",
+                        effectiveDate: "至2016年12月28日",
+                        //merchantPicture: "无",
+                        describe: "请各位小主们尽快下单吧~~"
+                    }}>
+                      <EditButton/>
+                      <SoldoutButton/>
+                    </CouponDetail>
+            </Page>
+        )
+    }
+}
+
+export default  ViewCouponDetail
