47971c855a572344476d1b6ae0f766156ba84460
liRenhao <renhao.li@bjyada.com>
2016-12-19 16:18:22 +0800

    创建action

--- src/action.js
+++ src/action.js
@@ -6,3 +6,23 @@
  * 用于描述界面中操作state中数据变化的action
  */
 import {createAction} from "redux-act"
+
+// 注册的请求action
+export const signUpRequest = createAction("SIGN_UP_REQUEST")
+
+// 登录的请求action
+export const loginRequest = createAction("LOGIN_REQUEST")
+// 更新Token的action
+export const updateToken = createAction("UPDATE_TOKEN")
+
+// 用户信息的请求action
+export const userInfoRequest = createAction("USER_INFO_REQUEST")
+// 更新用户信息的action
+export const updateUserInfo = createAction("UPDATE_USER_INFO")
+// 更新用户信息的请求action
+export const updateUserInfoRequest = createAction("UPDATE_USER_INFO_REQUEST")
+
+// 生成邀请码的请求action
+export const createInviteCodeRequest = createAction("CREATE_INVITE_CODE_REQUEST")
+// 更新邀请码的action
+export const updateInviteCode = createAction("UPDATE_INVITE_CODE")
