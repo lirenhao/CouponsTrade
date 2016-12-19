b15e173f0453a95655fdf6d263ed4783c5411df2
liRenhao <renhao.li@bjyada.com>
2016-12-19 16:01:56 +0800

    action文件提交

---
+++ src/action.js
@@ -0,0 +1,8 @@
+/**
+ * Author：liRenhao
+ * Create Date：2016/12/19
+ * Modified By：liRenhao
+ * Why & What is modified  <修改原因描述>
+ * 用于描述界面中操作state中数据变化的action
+ */
+import {createAction} from "redux-act"
