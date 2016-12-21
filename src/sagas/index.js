26173581e6b45599bf407b9e9be7148428e17f69
liRenhao <renhao.li@bjyada.com>
2016-12-21 08:24:24 +0800

    saga的异步请求等待

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -6,8 +6,18 @@
  * <文件描述>
  */
 import {takeEvery} from 'redux-saga'
-import {loginRequest, logoutRequest} from '../action'
-import {loginAsync, logoutAsync} from './async'
+import {
+    signUpRequest, loginRequest,
+    logoutRequest, getUserInfoRequest
+} from '../action'
+import {
+    signUpAsync, loginAsync,
+    logoutAsync, getUserInfoAsync
+} from './async'
+
+export function* watchSignUpRequest() {
+    yield* takeEvery(signUpRequest.getType(), signUpAsync)
+}
 
 export function* watchLoginRequest() {
     yield* takeEvery(loginRequest.getType(), loginAsync)
@@ -17,9 +27,15 @@ export function* watchLogoutRequest() {
     yield* takeEvery(logoutRequest.getType(), logoutAsync)
 }
 
+export function* watchGetUserInfoRequest() {
+    yield* takeEvery(getUserInfoRequest.getType(), getUserInfoAsync)
+}
+
 export default function* sagas() {
     yield [
+        watchSignUpRequest(),
         watchLoginRequest(),
-        watchLogoutRequest()
+        watchLogoutRequest(),
+        watchGetUserInfoRequest()
     ]
 }
