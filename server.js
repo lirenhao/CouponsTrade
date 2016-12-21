0c4958659595fc5e5784aac0e5da486bf5a5bd59
liRenhao <renhao.li@bjyada.com>
2016-12-21 08:57:26 +0800

    后台服务更新

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
  */
 import express from 'express'
 import bodyParser from 'body-parser'
+import {ServerPath, ResponseCode} from './src/constants'
 
 const app = express()
 app.use(bodyParser.json())
@@ -20,41 +21,82 @@ app.use(function (req, res, next) {
 
 const state = {
     token: "1234567890",
-    inviteCode: "InviteCodeB",
+    login: {
+        username: "1",
+        password: "1"
+    },
     userInfo: {
         nickname: "test",
         phoneNo: "18310809129",
         inviteCode: "InviteCodeA",
         inviteCount: 2
-    }
+    },
+    inviteCode: "InviteCodeB"
 }
 
-app.post('/signUp', function (req, res) {
+app.post(`/${ServerPath.SIGN_UP}`, function (req, res) {
     const {inviteCode} = req.body
     if (state.inviteCode == inviteCode)
-        res.json({code: "success"})
+        res.json({code: ResponseCode.SUCCESS})
     else
-        res.json({code: "field", msg: "邀请码错误"})
+        res.json({code: ResponseCode.FAIL, msg: "邀请码错误"})
 })
 
-app.post('/login', function (req, res) {
+app.post(`/${ServerPath.LOGIN}`, function (req, res) {
     const {username, password} = req.body
-    if (username == '1' && password == '1')
-        res.json({code: "success", token: state.token})
+    if (state.login.username == username && state.login.password == password)
+        res.json({code: ResponseCode.SUCCESS, token: state.token})
     else
-        res.json({code: "field", msg: "账号或密码错误"})
+        res.json({code: ResponseCode.FAIL, msg: "账号或密码错误"})
 })
 
-app.post('/logout', function (req, res) {
-    res.json({code: "success"})
+app.post(`/${ServerPath.LOGOUT}`, function (req, res) {
+    res.json({code: ResponseCode.SUCCESS})
 })
 
-app.post('/getUserInfo', function (req, res) {
+app.post(`/${ServerPath.GET_USER_INFO}`, function (req, res) {
     const {token} = req.body
     if (state.token == token)
-        res.json({code: "success", userInfo: state.userInfo})
+        res.json({code: ResponseCode.SUCCESS, userInfo: state.userInfo})
     else
-        res.json({code: "field", msg: "用户未登录"})
+        res.json({code: ResponseCode.FAIL, msg: "用户未登录"})
+})
+
+app.post(`/${ServerPath.UPDATE_USER_INFO}`, function (req, res) {
+    const {token, ...userInfo} = req.body
+    if (state.token == token) {
+        state.userInfo = userInfo
+        res.json({code: ResponseCode.SUCCESS})
+    } else
+        res.json({code: ResponseCode.FAIL, msg: "用户未登录"})
+})
+
+app.post(`/${ServerPath.CREATE_INVITE_CODE}`, function (req, res) {
+    const {token} = req.body
+    if (state.token == token) {
+        res.json({code: ResponseCode.SUCCESS, inviteCode: state.inviteCode})
+    } else
+        res.json({code: ResponseCode.FAIL, msg: "用户未登录"})
+})
+
+app.post(`/${ServerPath.VERIFY_PASSWORD}`, function (req, res) {
+    const {token, oldPassword} = req.body
+    if (state.token == token) {
+        if (state.login.password == oldPassword)
+            res.json({code: ResponseCode.SUCCESS})
+        else
+            res.json({code: ResponseCode.FAIL, msg: "密码错误"})
+    } else
+        res.json({code: ResponseCode.FAIL, msg: "用户未登录"})
+})
+
+app.post(`/${ServerPath.UPDATE_PASSWORD}`, function (req, res) {
+    const {token, newPassword} = req.body
+    if (state.token == token) {
+        state.login.password = newPassword
+        res.json({code: ResponseCode.SUCCESS})
+    } else
+        res.json({code: ResponseCode.FAIL, msg: "用户未登录"})
 })
 
 // TODO 添加后台服务
