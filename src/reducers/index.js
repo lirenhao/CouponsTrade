13ac376be328195ba4910320dbced57896cd969c
ALIENWARE17 <45544205@qq.com>
2016-12-21 08:52:31 +0800

    更新reducer,保证state的结构是设计的结构

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,12 @@ import token from './token'
 import loading from './loading'
 import inviteCode from './inviteCode'
 import userInfo from './userInfo'
+import orderList from './orderList'
+import orderInfo from './orderInfo'
+
+const order = combineReducers({orderList, orderInfo});
 
 export default combineReducers({
-    form, token, loading, inviteCode, userInfo
+    form, token, loading, inviteCode, userInfo,
+    order
 })
