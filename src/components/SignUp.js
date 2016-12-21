e8d2cfcb7d1b62e0e0f72571040be165bebd2a5e
upmiss <916888691@qq.com>
2016-12-21 08:59:24 +0800

    注册页面样式

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -7,7 +7,7 @@
  */
 import React from 'react'
 import {Field, reduxForm} from 'redux-form'
-import {Input, Button} from 'react-onsenui'
+import {Input, Button, List, ListItem} from 'react-onsenui'
 
 const SignUpField = ({input, type, placeholder}) => {
     return (
@@ -23,48 +23,60 @@ const SignUp = (props) => {
     const {handleSubmit, onSubmit, invalid, submitting} = props
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <section style={{textAlign: 'center'}}>
-                <p>
-                    <Field type="text"
-                           name="nickname"
-                           component={SignUpField}
-                           placeholder="输入昵称"/>
-                </p>
-                <p>
-                    <Field type="number"
-                           name="phoneNumber"
-                           component={SignUpField}
-                           placeholder="输入手机号"/>
-                </p>
-                <p>
-                    <Button style={{fontSize: '15px'}} modifier='quiet'>获取验证码</Button>
-                    <br/>
-                    <Field type="number"
-                           name="validateCode"
-                           component={SignUpField}
-                           placeholder="输入验证码"/>
-                </p>
-                <p>
-                    <Field type="password"
-                           name="password"
-                           component={SignUpField}
-                           placeholder="输入密码"/>
-                </p>
-                <p>
-                    <Field type="text"
-                           name="inviteCode"
-                           component={SignUpField}
-                           placeholder="输入邀请码"/>
-                </p>
-                <p style={{marginLeft: '20%', marginRight: '20%'}}>
-                    <br/>
-                    <Button modifier="large"
-                            disabled={invalid || submitting}
-                            onClick={props.submit}>
-                        注册
-                    </Button>
-                </p>
-            </section>
+            <List modifier="inset marginT marginB">
+                <ListItem>
+                    <div className="center">
+                        <Field type="text"
+                               name="nickname"
+                               component={SignUpField}
+                               placeholder="输入昵称"/>
+                    </div>
+                </ListItem>
+                <ListItem>
+                    <div className="center">
+                        <Field type="text"
+                               name="phoneNumber"
+                               component={SignUpField}
+                               placeholder="输入手机号"/>
+                    </div>
+                </ListItem>
+                <ListItem>
+                </ListItem>
+            </List>
+            <Button modifier='quiet marginTLR'>获取验证码</Button>
+            <List modifier="inset marginB">
+                <ListItem>
+                    <div className="center">
+                        <Field type="text"
+                               name="validateCode"
+                               component={SignUpField}
+                               placeholder="输入验证码"/>
+                    </div>
+                </ListItem>
+                <ListItem>
+                    <div className="center">
+                        <Field type="password"
+                               name="password"
+                               component={SignUpField}
+                               placeholder="输入密码"/>
+                    </div>
+                </ListItem>
+                <ListItem>
+                    <div className="center">
+                        <Field type="text"
+                               name="inviteCode"
+                               component={SignUpField}
+                               placeholder="输入邀请码"/>
+                    </div>
+                </ListItem>
+                <ListItem>
+                </ListItem>
+            </List>
+            <Button modifier="large marginTLR"
+                    disabled={invalid || submitting}
+                    onClick={props.submit}>
+                注册
+            </Button>
         </form>
     )
 }
