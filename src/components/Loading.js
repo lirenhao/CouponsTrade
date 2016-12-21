48e8b53a1c20d14e39f670ed5a5a0518cfb3c100
liRenhao <renhao.li@bjyada.com>
2016-12-21 09:21:04 +0800

    loading组件

---
+++ src/components/Loading.js
@@ -0,0 +1,27 @@
+/**
+ * Author：liRenhao
+ * Create Date：2016/12/21
+ * Modified By：liRenhao
+ * Why & What is modified  <修改原因描述>
+ * <文件描述>
+ */
+import React from 'react'
+import {Modal, ProgressCircular, Button} from 'react-onsenui'
+
+const Loading = (props) => {
+    return (
+        <Modal isOpen={props.loading}>
+            <section style={{margin: '16px'}}>
+                <p style={{opacity: 0.6}}>
+                    <ProgressCircular indeterminate/>
+                </p>
+            </section>
+        </Modal>
+    )
+}
+
+Loading.propTypes = {
+    loading: React.PropTypes.bool.isRequired
+}
+
+export default Loading
