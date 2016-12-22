9aac99ab34ac355349501cb83c926ce47d1d9343
liRenhao <renhao.li@bjyada.com>
2016-12-22 16:44:01 +0800

    用户信息展示组件回复Fab

--- src/components/UserShow.js
+++ src/components/UserShow.js
@@ -6,26 +6,39 @@
  * 用户信息展示组件
  */
 import React from 'react'
-import {Page, List, ListItem, ListHeader} from 'react-onsenui'
-
-
+import {SpeedDial, SpeedDialItem, Fab, Icon, List, ListItem, ListHeader} from 'react-onsenui'
 
 const UserShow = (props) => {
     const {share, edit, user} = props
     return (
-        <List modifier='inset marginT'>
-            <ListHeader><img src={`http://placekitten.com/g/40/40`} alt=""/></ListHeader>
-            <ListItem>
-                昵称 <div className="right">{user.nickname}</div>
-            </ListItem>
-            <ListItem>
-                手机号<div className="right">{user.phoneNumber}</div>
-            </ListItem>
-            <ListItem>
-                邀请码<div className="right">{user.inviteCode}</div>
-            </ListItem>
-        </List>
-
+        <div>
+            <SpeedDial position='bottom right'>
+                <Fab>
+                    <Icon icon='md-menu'/>
+                </Fab>
+                <SpeedDialItem onClick={share}>
+                    <Icon icon='md-share'/>
+                </SpeedDialItem>
+                <SpeedDialItem onClick={edit}>
+                    <Icon icon='md-edit'/>
+                </SpeedDialItem>
+            </SpeedDial>
+            <List modifier='inset marginT'>
+                <ListHeader><img src={`http://placekitten.com/g/40/40`} alt=""/></ListHeader>
+                <ListItem>
+                    昵称
+                    <div className="right">{user.nickname}</div>
+                </ListItem>
+                <ListItem>
+                    手机号
+                    <div className="right">{user.phoneNumber}</div>
+                </ListItem>
+                <ListItem>
+                    邀请码
+                    <div className="right">{user.inviteCode}</div>
+                </ListItem>
+            </List>
+        </div>
     )
 }
 
