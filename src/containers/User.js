dc88b99ca12b5fe4caa7ad2515c67b33f2f16db2
liRenhao <renhao.li@bjyada.com>
2016-12-23 14:22:57 +0800

    用户详情添加右侧菜单

--- src/containers/User.js
+++ src/containers/User.js
@@ -7,7 +7,19 @@
  */
 import React from "react";
 import {connect} from "react-redux";
-import {Page, Toolbar, BackButton, Button} from "react-onsenui";
+import {
+    Splitter,
+    SplitterSide,
+    SplitterContent,
+    List,
+    ListItem,
+    Page,
+    Toolbar,
+    BackButton,
+    ToolbarButton,
+    Icon,
+    Button
+} from "react-onsenui";
 import {updateUserInfoRequest, createInviteCodeRequest} from "../action";
 import UserShow from "../components/UserShow";
 import UserEdit from "../components/UserEdit";
@@ -40,28 +52,74 @@ const Share = (props) => {
 }
 
 class User extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    hide() {
+        this.setState({isOpen: false})
+    }
+
+    show() {
+        this.setState({isOpen: true})
+    }
+
     render() {
         return (
-            <Page renderToolbar={() => (
-                <Toolbar>
-                    <div className='left'><BackButton/></div>
-                    <div className='center'>个人信息</div>
-                </Toolbar>
-            )}>
-                <UserShow
-                    edit={() => this.props.navigator.pushPage({
-                        comp: Edit,
-                        props: {
-                            key: 'userEdit',
-                            user: this.props.userInfo,
-                            userEdit: (param) => {
-                                this.props.updateUserInfo({...param, token: this.props.token}, this.props.navigator)
-                            }
-                        }
-                    })}
-                    share={() => this.props.createInviteCode(this.props.token, this.props.navigator)}
-                    user={this.props.userInfo}
-                />
+            <Page>
+                <Splitter>
+                    <SplitterSide
+                        side='right'
+                        width={200}
+                        collapse={true}
+                        isSwipeable={true}
+                        isOpen={this.state.isOpen}
+                        onClose={this.hide.bind(this)}
+                        onOpen={this.show.bind(this)}
+                    >
+                        <Page>
+                            <List>
+                                <ListItem appable>信息编辑</ListItem>
+                                <ListItem appable>重置密码</ListItem>
+                                <ListItem appable>生成邀请码</ListItem>
+                            </List>
+                        </Page>
+                    </SplitterSide>
+                    <SplitterContent>
+                        <Page renderToolbar={() => (
+                            <Toolbar>
+                                <div className='left'><BackButton/></div>
+                                <div className='center'>个人信息</div>
+                                <div className='right'>
+                                    <ToolbarButton onClick={this.show.bind(this)}>
+                                        <Icon icon='md-menu'/>
+                                    </ToolbarButton>
+                                </div>
+                            </Toolbar>
+                        )}>
+                            <UserShow
+                                edit={() => this.props.navigator.pushPage({
+                                    comp: Edit,
+                                    props: {
+                                        key: 'userEdit',
+                                        user: this.props.userInfo,
+                                        userEdit: (param) => {
+                                            this.props.updateUserInfo({
+                                                ...param,
+                                                token: this.props.token
+                                            }, this.props.navigator)
+                                        }
+                                    }
+                                })}
+                                share={() => this.props.createInviteCode(this.props.token, this.props.navigator)}
+                                user={this.props.userInfo}
+                            />
+                        </Page>
+                    </SplitterContent>
+                </Splitter>
             </Page>
         )
     }
