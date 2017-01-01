44ff3ae70b55e3c2ce4ef2192319a71370a47d80
Yky <45544205@qq.com>
2017-01-01 12:24:41 +0800

    添加取消订单功能

--- src/constants/index.js
+++ src/constants/index.js
@@ -43,35 +43,36 @@ export const GET_ORDER_INFO_REQUEST = 'GET_ORDER_INFO_REQUEST'
 export const SET_ORDER_INFO = 'SET_ORDER_INFO'
 export const OPEN_COUPON_REQUEST = 'OPEN_COUPON_REQUEST'
 export const OPEN_COUPON = 'OPEN_COUPON'
+export const REFRESH_ORDER_LIST_REQUEST = 'REFRESH_ORDER_LIST_REQUEST'
 
 // 服务器地址
 export const ServerPath = {
-    LOGIN: 'login',
-    LOGOUT: 'logout',
-    SIGN_UP: 'signUp',
-    GET_USER_INFO: 'getUserInfo',
-    UPDATE_USER_INFO: 'updateUserInfo',
-    CREATE_INVITE_CODE: 'createInviteCode',
-    VERIFY_PASSWORD: 'verifyPassword',
-    UPDATE_PASSWORD: 'updatePassword',
-    PUBLISH_COUPON: 'publishCoupon',
-    GET_COUPON_DETAILS: 'getCouponDetails',
-    GET_USER_COUPONS: 'getUserCoupons',
-    EDIT_USER_COUPON: 'editUserCoupon',
-    CREATE_ORDER: 'createOrder',
-    GET_ORDER_LIST: 'getOrderList',
-    INSET_ORDER_LIST: 'insertOrderList',
-    GET_ORDER_INFO: 'getOrderInfo',
-    PAY: 'pay',
-    OPEN_COUPON: 'openCoupon',
-    CANCEL_ORDER: 'cancelOrder',
-    QUERY_COUPONS: 'queryCoupons',
-    SOLD_OUT_COUPON: 'soldOutCoupon',
+	LOGIN: 'login',
+	LOGOUT: 'logout',
+	SIGN_UP: 'signUp',
+	GET_USER_INFO: 'getUserInfo',
+	UPDATE_USER_INFO: 'updateUserInfo',
+	CREATE_INVITE_CODE: 'createInviteCode',
+	VERIFY_PASSWORD: 'verifyPassword',
+	UPDATE_PASSWORD: 'updatePassword',
+	PUBLISH_COUPON: 'publishCoupon',
+	GET_COUPON_DETAILS: 'getCouponDetails',
+	GET_USER_COUPONS: 'getUserCoupons',
+	EDIT_USER_COUPON: 'editUserCoupon',
+	CREATE_ORDER: 'createOrder',
+	GET_ORDER_LIST: 'getOrderList',
+	INSET_ORDER_LIST: 'insertOrderList',
+	GET_ORDER_INFO: 'getOrderInfo',
+	PAY: 'pay',
+	OPEN_COUPON: 'openCoupon',
+	CANCEL_ORDER: 'cancelOrder',
+	QUERY_COUPONS: 'queryCoupons',
+	SOLD_OUT_COUPON: 'soldOutCoupon',
 };
 
 // 响应码
 export const ResponseCode = {
-    SUCCESS: 'success',
-    FAIL: 'fail',
-    TIMEOUT: 'timeout'
+	SUCCESS: 'success',
+	FAIL: 'fail',
+	TIMEOUT: 'timeout'
 };
