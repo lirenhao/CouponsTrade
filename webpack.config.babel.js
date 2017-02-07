6ba5200519413799c8737c773475a3ee9456af19
ALIENWARE17 <45544205@qq.com>
2017-02-07 09:33:36 +0800

    webpack升级到v2版本

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,43 +6,42 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import OpenBrowserPlugin from 'open-browser-webpack-plugin'
 
 export default {
-    entry: {
-        bundle: ['react-hot-loader/patch',
-            'babel-polyfill',
-            './src/index.js'],
-    },
-    output: {
-        filename: '[name].js',
-        path: 'build',
-        publicPath: "build/"
-    },
-    module: {
-        loaders: [
-            {test: /\.jsx?$/, loader: 'babel-loader', exclude: path.resolve(__dirname, 'node_modules')},
-            {test: /\.css$/, loaders: ['style-loader', 'css-loader']},
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
-            {
-                test: /\.(woff|woff2)(\?v=\d\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=application/font-woff"
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=application/octet-stream"
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=image/svg+xml"
-            },
-            {test: /\.json$/, loader: "json"}
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "优惠券交易平台",
-        }),
-        new OpenBrowserPlugin({
-            url: "http://localhost:8080"
-        }),
-    ],
-    devtool: "eval"
+  entry: {
+    bundle: ['react-hot-loader/patch',
+      'babel-polyfill',
+      './src/index.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: '/build',
+    publicPath: "/build"
+  },
+  module: {
+    rules: [
+      {test: /\.jsx?$/, use: 'babel-loader', exclude: path.resolve(__dirname, 'node_modules')},
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: "file-loader"},
+      {
+        test: /\.(woff|woff2)(\?v=\d\.\d+\.\d+)?$/,
+        use: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        use: "url-loader?limit=10000&mimetype=application/octet-stream"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: "url-loader?limit=10000&mimetype=image/svg+xml"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "优惠券交易平台",
+    }),
+    new OpenBrowserPlugin({
+      url: "http://localhost:8080"
+    }),
+  ],
+  devtool: "eval"
 }
