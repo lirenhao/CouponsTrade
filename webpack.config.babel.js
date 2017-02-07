6bb637ce64920aa29edd1e82d78954d1cfabb392
liRenhao <renhao.li@bjyada.com>
2017-02-07 17:37:07 +0800

    webpack配置打包后的路径

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,42 +6,46 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import OpenBrowserPlugin from 'open-browser-webpack-plugin'
 
 export default {
-  entry: {
-    bundle: ['react-hot-loader/patch',
-      'babel-polyfill',
-      './src/index.js'],
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: "build/"
-  },
-  module: {
-    rules: [
-      {test: /\.jsx?$/, use: 'babel-loader', exclude: path.resolve(__dirname, 'node_modules')},
-      {test: /\.css$/, use: ['style-loader', 'css-loader']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: "file-loader"},
-      {
-        test: /\.(woff|woff2)(\?v=\d\.\d+\.\d+)?$/,
-        use: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use: "url-loader?limit=10000&mimetype=application/octet-stream"
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: "url-loader?limit=10000&mimetype=image/svg+xml"
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "优惠券交易平台",
-    }),
-    new OpenBrowserPlugin({
-      url: "http://localhost:8080"
-    }),
-  ],
-  devtool: "eval"
+    entry: {
+        bundle: ['react-hot-loader/patch',
+            'babel-polyfill',
+            './src/index.js'],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'build/dist'),
+        publicPath: "dist/"
+    },
+    module: {
+        rules: [
+            {test: /\.jsx?$/, use: 'babel-loader', exclude: path.resolve(__dirname, 'node_modules')},
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: "file-loader"},
+            {
+                test: /\.(woff|woff2)(\?v=\d\.\d+\.\d+)?$/,
+                use: "url-loader?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                use: "url-loader?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: "url-loader?limit=10000&mimetype=image/svg+xml"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "优惠券交易平台",
+            filename: '../index.html'
+        }),
+        new OpenBrowserPlugin({
+            url: "http://localhost:8080"
+        }),
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, "build"),
+    },
+    devtool: "eval"
 }
