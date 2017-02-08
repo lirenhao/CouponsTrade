9325d77d2f04846a3c074a4d2fee045aa75e5a98
ALIENWARE17 <45544205@qq.com>
2017-02-08 15:32:12 +0800

    修改打包路径，用npm start可以直接运行， 打开打包后的index.html也可以直接运行

--- src/fetch.js
+++ src/fetch.js
@@ -5,25 +5,25 @@
  * Why & What is modified  <修改原因描述>
  * 向后台发送数据
  */
-import fetch from 'node-fetch'
+import  'whatwg-fetch'
 import {ResponseCode} from './constants'
 
 export default (path, param = {}) => {
-    const content = Object.keys(param)
-        .map((key) => key + '=' + param[key])
-        .reduce((pre, cur) => pre + '&' + cur, "")
-    const options = {
-        method: 'post',
-        body: content,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
+  const content = Object.keys(param)
+    .map((key) => key + '=' + param[key])
+    .reduce((pre, cur) => pre + '&' + cur, "")
+  const options = {
+    method: 'post',
+    body: content,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
     }
-    return fetch(`http://localhost:3000/${path}`, options)
-        .then(function (res) {
-            return res.json()
-        })
-        .catch(function (e) {
-            return {code: ResponseCode.TIMEOUT, msg: "请求超时"}
-        })
+  }
+  return fetch(`http://localhost:3000/${path}`, options)
+    .then(function (res) {
+      return res.json()
+    })
+    .catch(function (e) {
+      return {code: ResponseCode.TIMEOUT, msg: "请求超时"}
+    })
 }
