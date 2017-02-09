a2d3066a3dfa5dc621f185e6d000f5b5a24851a2
liRenhao <renhao.li@bjyada.com>
2017-02-09 10:04:22 +0800

    请求完成后的处理更正为call

--- src/sagas/async.js
+++ src/sagas/async.js
@@ -10,56 +10,56 @@ import * as Act from '../actions'
 import {ResponseCode} from '../constants'
 
 export function* request(api) {
-  while (true) {
-    let action = yield take((action) => /.*REQUEST/.test(action.type))
-    let {apiType, param, router, handle, callback} = action.payload
-    handle = {
-      before: [Act.onload],
-      success: [(res) => ({type: action.type.replace(/REQUEST/, 'SUCCESS'), payload: res})],
-      fail: [Act.showDialog],
-      exception: [Act.showDialog],
-      after: [Act.unload],
-      ...handle
-    }
-    callback = {
-      before: [],
-      success: [],
-      fail: [],
-      exception: [],
-      after: [],
-      ...callback
-    }
-    // 请求之前的处理
-    yield [callbackHandle(handle.before, callback.before)]
-    try {
-      let res = yield call(api, apiType, param)
-      console.log("--------------------------------------")
-      console.log(res)
-      console.log("--------------------------------------")
-      if (res.code == ResponseCode.SUCCESS) {
-        // 成功后的处理
-        yield [callbackHandle(handle.success, callback.success, res)]
-        if (router) {
-          yield call(router, res)
+    while (true) {
+        let action = yield take((action) => /.*REQUEST/.test(action.type))
+        let {apiType, param, router, handle, callback} = action.payload
+        handle = {
+            before: [Act.onload],
+            success: [(res) => ({type: action.type.replace(/REQUEST/, 'SUCCESS'), payload: res})],
+            fail: [Act.showDialog],
+            exception: [Act.showDialog],
+            after: [Act.unload],
+            ...handle
         }
-      } else {
-        // 失败后的处理
-        yield [callbackHandle(handle.fail, callback.fail, res.msg)]
-      }
-    } catch (e) {
-      // 异常的处理
-      yield [callbackHandle(handle.exception, callback.exception, e.message)]
+        callback = {
+            before: [],
+            success: [],
+            fail: [],
+            exception: [],
+            after: [],
+            ...callback
+        }
+        // 请求之前的处理
+        yield call(callbackHandle, handle.before, callback.before)
+        try {
+            let res = yield call(api, apiType, param)
+            console.log("--------------------------------------")
+            console.log(res)
+            console.log("--------------------------------------")
+            if (res.code == ResponseCode.SUCCESS) {
+                // 成功后的处理
+                yield call(callbackHandle, handle.success, callback.success, res)
+                if (router) {
+                    yield call(router, res)
+                }
+            } else {
+                // 失败后的处理
+                yield call(callbackHandle, handle.fail, callback.fail, res.msg)
+            }
+        } catch (e) {
+            // 异常的处理
+            yield call(callbackHandle, handle.exception, callback.exception, e.message)
+        }
+        // 请求之后的处理
+        yield call(callbackHandle, handle.after, callback.after)
     }
-    // 请求之后的处理
-    yield [callbackHandle(handle.after, callback.after)]
-  }
 }
 
 function* callbackHandle(actCreates, funcs, payload) {
-  for (let func of funcs) {
-    yield call(func, payload)
-  }
-  for (let actCreate of actCreates) {
-    yield put(actCreate(payload))
-  }
+    for (let func of funcs) {
+        yield call(func, payload)
+    }
+    for (let actCreate of actCreates) {
+        yield put(actCreate(payload))
+    }
 }
